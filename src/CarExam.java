dff98d6b929b798c7b70c2074d13b201e2f1ab05
coolman0123 <socool4m@naver.com>
2024-09-10 19:53:00 +0900

    java

--- src/CarExam.java
+++ src/CarExam.java
@@ -1,2 +1,20 @@
-package com.example;public class CarExam {
+public class CarExam {
+    public static void main(String[] args){
+        Car c2 = new Bus();
+        //Car를 상속 받는데 클래스를 만들고 싶진 않다.
+
+        CarAbst c1 = new CarAbst(){
+
+            @Override
+            public void a() {
+                System.out.println("이름 없는 객체 a() 메소드 오버라이딩");
+            }
+        };
+
+
+
+
+        c1.a();
+
+    }
 }
