924a394ff495d822176ac0e8b7fecd882ab90a11
coolman0123 <socool4m@naver.com>
2024-09-20 21:05:59 +0900

    중요 깊은 복사와 얉은 복사의 차이

---
+++ src/Array14.java
@@ -0,0 +1,28 @@
+
+public class Array14 {
+    public static void main(String[] args) {
+
+        int[] copyFrom = {1, 2, 3};
+        int[] copyTo = java.util.Arrays.copyOf(copyFrom, copyFrom.length);
+
+        for(int i: copyTo){
+            System.out.println(i);
+        }
+        System.out.println("-------------------------");
+        int[] copyTo2 = java.util.Arrays.copyOf(copyFrom, 5);
+        // 길이를 초과하게 copy한다면 0값을 받게 함
+        for(int j: copyTo2){
+            System.out.println(j);
+        }
+
+        int[] copyTo3 = copyFrom;
+        // 1과 3은 뭐가 다를까?
+        // 3이라면 같은 걸 참조한다는 뜻임 copyFrom == copyTo3
+        // 1은? 새로운 배열을 만들어서 copyTo가 참조시킴 메모리 속에서는 각자 다른 배열인거다.
+        System.out.println("-------------------------");
+        for(int c : copyTo3){
+            System.out.println(c);
+        }
+
+    }
+}
