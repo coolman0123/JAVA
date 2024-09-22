c95a9ac40f5718ff9d7bdf11763e8193b1ef8c5a
coolman0123 <socool4m@naver.com>
2024-09-22 22:47:37 +0900

    배열 정렬 sort

---
+++ src/Array17.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Array17 {
+    public static void main(String[] args) {
+        int[] array = {3,4,1,2,5};
+
+        Arrays.sort(array);
+
+        for(int i : array){
+            System.out.println(i);
+        }
+    }
+}
