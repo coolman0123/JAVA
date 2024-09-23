94153a39c5dfa205ffc9af1a03755056ad7e695e
coolman0123 <socool4m@naver.com>
2024-09-23 23:05:38 +0900

    추상화

---
+++ src/com/example/sample/Abstraction.java
@@ -0,0 +1,18 @@
+package com.example.sample;
+
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Shape shape = new Circle();
+        shape.draw();
+    }
+}
